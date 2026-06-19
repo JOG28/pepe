33d6d3f198536500a372a327a1baa1a0b1a7dcc9
“Jorge <jogeosuna@hotmail.com>
2026-06-19 08:58:47 -0700

    fix: usar base de datos emyg en produccion

--- set-env.js
+++ set-env.js
@@ -5,8 +5,8 @@ const targetPath = './src/environments/environment.prod.ts';
 const envConfigFile = `
 export const environment = {
   production: true,
-  supabaseUrl: 'https://yzhraaiydfkaypnkliin.supabase.co',
-  supabaseKey: 'sb_publishable__6Ye1i3pO_fmDufIPwxIuA_smXCDuGE',
+  supabaseUrl: 'https://emygdjxixfhaurxofyel.supabase.co',
+  supabaseKey: 'sb_publishable_TcXkD2bADJLkBv19ZwNUJg_b6gXWXmx',
   geminiApiKey: '${process.env.GEMINI_API_KEY || ''}',
   openRouterApiKey: ''
 };
