4512d98059d27e67f9ad016f972282bfaff59b91
“Jorge <jogeosuna@hotmail.com>
2026-06-24 08:06:59 -0700

    nuevo deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   supabaseKey: 'sb_publishable_TcXkD2bADJLkBv19ZwNUJg_b6gXWXmx',
 
   // Google Gemini
-  geminiApiKey: 'AQ.Ab8RN6JLBtN_JquMjjOLBHfSptgNFj1UdP2xJArB5CF8kUnBeQ',
+  geminiApiKey: 'AQ.Ab8RN6JAtUnwWGbC0C2rRS6FTGyjihxyS7lvDE13Tph_Gk0nCA',
 
   // Legacy (no usado)
   openRouterApiKey: ''
